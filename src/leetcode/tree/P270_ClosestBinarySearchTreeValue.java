15d7168f7392a7ac4e8a3dd39d5f4690e7efaea9
Vishal Mittal <vishalpmittal@gmail.com>
2016-07-18 16:41:43 -0700

    Adding more Leetcode problems
    Adding more Leetcode problems

---
+++ src/leetcode/tree/P270_ClosestBinarySearchTreeValue.java
@@ -0,0 +1,31 @@
+/**
+ * Given a non-empty binary search tree and a target value, 
+ * find the value in the BST that is closest to the target.
+ * 
+ * Note:
+ * -  Given target value is a floating point.
+ * -  You are guaranteed to have only one unique value in the BST that 
+ * is closest to the target.
+ */
+
+package leetcode.tree;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import leetcode.dependencies.TreeNode;
+
+public class P270_ClosestBinarySearchTreeValue {
+	
+    public static int closestValue(TreeNode root, double target) {
+        
+    	
+    	return 0;
+    }
+    
+	public static void main(String[] args) {
+//		assertTrue("Test1", problem(94));
+//		assertFalse("Test2", problem(97));
+		System.out.println("All Tests passed");
+	}
+}
