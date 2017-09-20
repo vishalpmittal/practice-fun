5513c9e55d6d188eb40b64780a9e061d4d200b84
vishalm <vishalm@vmware.com>
2017-09-20 13:54:10 -0700

    addming more leetcode problems solved

---
+++ funNLearn/src/main/java/leetcode/P557_ReverseWordsInAStringIII.java
@@ -0,0 +1,35 @@
+/**
+ * Given a string, you need to reverse the order of characters in each word 
+ * within a sentence while still preserving whitespace and initial word order.
+ * 
+ * Example 1:
+ * Input: "Let's take LeetCode contest"
+ * Output: "s'teL ekat edoCteeL tsetnoc"
+ * 
+ * Note: In the string, each word is separated by single space and there will 
+ * not be any extra space in the string.
+ */
+
+package leetcode;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class P557_ReverseWordsInAStringIII {
+
+    public static String reverseWords(String s) {
+        
+        
+        return null;
+    }
+
+    public static boolean problem(int n) {
+        return problem_1(n);
+    }
+
+    public static void main(String[] args) {
+        assertTrue("Test1", problem(94));
+        assertFalse("Test2", problem(97));
+        System.out.println("All Tests passed");
+    }
+}
