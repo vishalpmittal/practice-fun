b8a46b7430390680e88334a6f2e67bf825d59a14
vishalm <vishalm@vmware.com>
2018-02-07 12:12:40 -0800

    Angular service basics

--- courses/FullStackWebDev2/02AngularJS/Angular/conFusion/src/app/menu/menu.component.ts
+++ courses/FullStackWebDev2/02AngularJS/Angular/conFusion/src/app/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes';
+
+import { DishService } from '../services/dish.service';
 
 @Component({
   selector: 'app-menu',
@@ -10,12 +11,14 @@ import { DISHES } from '../shared/dishes';
 
 export class MenuComponent implements OnInit {
 
-  dishes = DISHES;
+  dishes: Dish[];
   selectedDish: Dish;
 
-  constructor() { }
+  constructor(private dishService: DishService) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.dishes = this.dishService.getDishes();
+  }
 
   onSelect(dish: Dish) {
     this.selectedDish = dish;
