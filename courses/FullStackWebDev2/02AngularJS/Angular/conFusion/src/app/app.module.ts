5956de66c8018e7a93dca04057250a92bc30fe62
vishalm <vishalm@vmware.com>
2018-04-04 13:47:29 -0700

    FSWD2 Attribute Directives

--- courses/FullStackWebDev2/02AngularJS/Angular/conFusion/src/app/app.module.ts
+++ courses/FullStackWebDev2/02AngularJS/Angular/conFusion/src/app/app.module.ts
@@ -30,6 +30,7 @@ import { processHTTPMsgService } from './services/process-httpmsg.service';
 
 import { RestangularModule, Restangular } from 'ngx-restangular';
 import { RestangularConfigFactory } from './shared/restConfig';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -41,7 +42,8 @@ import { RestangularConfigFactory } from './shared/restConfig';
     AboutComponent,
     HomeComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   entryComponents: [
     LoginComponent
