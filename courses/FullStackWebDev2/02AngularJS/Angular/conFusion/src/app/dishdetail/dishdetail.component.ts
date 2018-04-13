644cb05250d54229e464f516f29d40148dabbdb4
vishalm <vishalm@vmware.com>
2018-04-13 15:57:10 -0700

    FSWD2, Angular Animations Part1

--- courses/FullStackWebDev2/02AngularJS/Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ courses/FullStackWebDev2/02AngularJS/Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -8,11 +8,25 @@ import { DishService } from '../services/dish.service';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import 'rxjs/add/operator/switchMap';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+        state('shown', style({
+            transform: 'scale(1.0)',
+            opacity: 1
+        })),
+        state('hidden', style({
+            transform: 'scale(0.5)',
+            opacity: 0
+        })),
+        transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 
 export class DishdetailComponent implements OnInit {
@@ -26,6 +40,7 @@ export class DishdetailComponent implements OnInit {
   next: number;
   ddAuthorComment: Comment;
   errMess: string;
+  visibility = 'shown';
 
   ddFormErrors = {
     'author': '',
@@ -53,12 +68,16 @@ export class DishdetailComponent implements OnInit {
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params
-      .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
-      .subscribe(
+      .switchMap((params: Params) => {
+          this.visibility = 'hidden';
+          return this.dishservice.getDish(+params['id']);
+        }
+      ).subscribe(
           dish => {
             this.dish = dish;
             this.dishcopy = dish;
             this.setPrevNext(dish.id);
+            this.visibility = 'shown';
           },
           errmess => this.errMess = <any>errmess
       );
