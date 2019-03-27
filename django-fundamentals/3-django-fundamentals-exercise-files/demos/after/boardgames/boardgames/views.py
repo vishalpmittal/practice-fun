ac86cd276505b826cb28b2051aeb5c2dc9af8963
vishalm <vishalm@vmware.com>
2019-03-27 15:58:44 -0700

    rearranged django and added django course about templates, forms, meta, jquery, user authentication

--- django-fundamentals/3-django-fundamentals-exercise-files/demos/after/boardgames/boardgames/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic.base import TemplateView
-from datetime import date
-
-
-class HelloWorldView(TemplateView):
-    template_name="helloworld.html"
-    
-    def get_context_data(self, **kwargs):
-        return {'date': date.today()}
